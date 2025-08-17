e9b05812b3ca65338fe615b06b3eed2d8c0761e8
ddlawton <ddlawton1@gmail.com>
2025-08-17 15:23:13 -0400

    manuscript figure edits

---
+++ R/manuscript_figure_creation/figure_2.R
@@ -0,0 +1,15 @@
+
+library(tidyverse)
+library(ggpubr)
+library(patchwork)
+library(here)
+library(mgcv)
+library(knitr)
+library(kableExtra)
+
+# --- Set Root Directory for Project ---
+here::i_am("README.md")  # Anchor project for reproducible paths
+
+# --- read in data ---
+
+
